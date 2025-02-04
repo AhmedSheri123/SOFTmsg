d19c2bf31a4b7e4c6c807a32174b12b10301dd74
AhmedSheri <85445168+AhmedSheri123@users.noreply.github.com>
2025-02-04 15:39:55 +0300

    ss

--- dashboard/views.py
+++ dashboard/views.py
@@ -355,7 +355,7 @@ def AddPatientManagementSettings(request, id):
             post_data['subscription_scope'] = user_service.plan_scope
 
             res = requests.post(f'{PatientManagementURL}/en/accounts/AddHospitalsByAPI', data=post_data)
-            messages.error(request, str(res, res.text))
+            messages.error(request, str(res.status_code)+str(res.text))
             if res.status_code == 200:
                 res_data = res.json()
                 
