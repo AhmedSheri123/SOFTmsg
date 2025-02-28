a8371841c237f82e9eecb414a942ba4dafff5361
AhmedSheri <85445168+AhmedSheri123@users.noreply.github.com>
2025-02-28 08:53:50 +0300

    resources empty field fixed

--- resources/sitemaps.py
+++ resources/sitemaps.py
@@ -4,8 +4,6 @@ from django.utils.translation import activate, get_language
 from django.conf import settings
 from .models import DocsServicesModel, DocsServiceSectionsModel, SectionContentsModel
 
-
-
 class MultiLangContentSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
@@ -15,10 +13,13 @@ class MultiLangContentSitemap(Sitemap):
         items = []
         for obj in SectionContentsModel.objects.filter(is_enabled=True, is_default_selected=False):
             for lang_code, _ in settings.LANGUAGES:
-                items.append((obj, lang_code))
+                desc_attr = f'desc_{lang_code}'
+                if getattr(obj, desc_attr, None):
+                    items.append((obj, lang_code))
         return items
 
     def location(self, item):
+        
         """إرجاع الرابط بناءً على اللغة"""
         obj, lang_code = item
         return f"/{lang_code}/resources/{obj.section.service.pk}?content_id={obj.pk}"
--- resources/templatetags/resources_tags.py
+++ resources/templatetags/resources_tags.py
@@ -2,6 +2,7 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from resources.models import SectionContentsModel, DocsServicesModel
 from django.contrib.auth.models import User
+from django.utils.translation import activate, get_language
 
 register = template.Library()
 
@@ -9,6 +10,10 @@ register = template.Library()
 @stringfilter
 def get_contents_by_section(section_id):
     contents = SectionContentsModel.objects.filter(section__id=section_id)
+    for content in contents:
+        lang = get_language()
+        if not getattr(content, f'name_{lang}', None):
+            contents = contents.exclude(id=content.id)
     return contents
 
 
@@ -16,4 +21,8 @@ def get_contents_by_section(section_id):
 @stringfilter
 def get_doc_services(section_id):
     doc_services = DocsServicesModel.objects.filter()
+    for doc_service in doc_services:
+        lang = get_language()
+        if not getattr(doc_service, f'name_{lang}', None):
+            doc_services = doc_services.exclude(id=doc_service.id)
     return doc_services
